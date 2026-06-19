667e470dc87460b4606e3756fbe51a82ac9b9669
Anvar <soxibovanvar1009@gmail.com>
2026-06-19 12:16:32 +0500

    Add project files.

---
+++ LeetCode_Task/Program.cs
@@ -0,0 +1,30 @@
+﻿using LeetCode_Task;
+
+int[] nums = [3, 2, 4];
+int target = 6;
+
+int [] index= Solution1.TwoSum(nums, target);
+
+for (int i = 0; i < index.Length; i++)
+{
+    Console.WriteLine(index[i]);
+}
+
+public static class Solution1
+{
+    public static int[] TwoSum(int[] nums, int k)
+    {
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = +1; j < nums.Length; j++)
+            {
+                if (nums[i] + nums[j] == k)
+                {
+                    return new int[] { i, j };
+                }
+
+            }
+        }
+        return null;
+    }
+}
---
+++ LeetCode_Task/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_Task
+{
+    public static class Solution
+    {
+        public static int[] TwoSum(int[] nums, int k)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for( int j = i; j < nums.Length; j++)
+                {
+                    if(nums[i] + nums[j] == k)
+                    {
+                        return new int[] { i, j };
+                    }
+
+                }
+            }
+            return null;
+        }
+    }
+}
