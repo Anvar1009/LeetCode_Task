ec8b7a1df9ee21b71226bb98031c2241c6c1a3ae
Anvar <soxibovanvar1009@gmail.com>
2026-06-30 12:41:05 +0500

    PlusOne completed

--- LeetCode_Task/Program.cs
+++ LeetCode_Task/Program.cs
@@ -1,4 +1,5 @@
 ﻿using LeetCode_Task;
+using System.Numerics;
 /* //N1
 int[] nums = [3, 2, 4];
 int target = 6;
@@ -11,10 +12,15 @@ for (int i = 0; i < index.Length; i++)
 }
 */
 
-int a = 11;
 
-bool b=Solution1.IsPalindrome(a);
-Console.WriteLine(b);
+
+int[] digits = { 7, 2, 8, 5, 0, 9, 1, 2, 9, 5, 3, 6, 6, 7, 3, 2, 8, 4, 3, 7, 9, 5, 7, 7, 4, 7, 4, 9, 4, 7, 0, 1, 1, 1, 7, 4, 0, 0, 6 };
+
+var result = Solution1.PlusOne(digits);
+for (int i = 0; i < result.Length; i++)
+{
+    Console.WriteLine(result[i]);
+}
 
 public static class Solution1
 {
@@ -84,7 +90,24 @@ public static class Solution1
 
 
 
+    public static int[] PlusOne(int[] digits)
+    {
+        for (int i = digits.Length - 1; i >= 0; i--)
+        {
+            if (digits[i] < 9)
+            {
+                digits[i]++;
+                return digits;
+            }
 
+            digits[i] = 0;
+        }
+
+        int[] result = new int[digits.Length + 1];
+        result[0] = 1;
+        return result;
+
+    }
 
 
 
